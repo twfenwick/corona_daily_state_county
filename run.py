f3b4d003bd351f4d82959ecac6f7dd335db89c76
tim fenwick <twfenwick@gmail.com>
2020-04-03 01:44:31 -0400

    Add log plot option

--- run.py
+++ run.py
@@ -8,7 +8,8 @@ path_states = '/Users/tim/code/github/nytimes/covid-19-data/us-states.csv'
 
 cmdln_override = False
 config_d = False
-config_od = True
+config_od = False
+config_logy = True
 
 """
 When I enter county and state
@@ -19,7 +20,7 @@ Then I see a trend chart of the state infection rate
 """
 
 
-def pandafunc(state='North Carolina', county=None, deaths=False, only_deaths=False):
+def pandafunc(state='North Carolina', county=None, deaths=False, only_deaths=False, logy=False):
     match = [state, county]
     path = path_counties if county else path_states
     df = pandas.read_csv(path)
@@ -34,6 +35,7 @@ def pandafunc(state='North Carolina', county=None, deaths=False, only_deaths=Fal
     if cmdln_override:
         only_deaths = config_od
         deaths = config_d
+        logy = config_logy
 
     if deaths:
         count = ['cases', 'deaths']
@@ -42,7 +44,7 @@ def pandafunc(state='North Carolina', county=None, deaths=False, only_deaths=Fal
     else:
         count = 'cases'
 
-    df.plot(kind='bar', x='date', y=count, grid=True, title=title)
+    df.plot(logy=logy, kind='bar', x='date', y=count, grid=True, title=title)
     plt.show()
     plt.close()
 
@@ -53,5 +55,7 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--county', help='Optional for specific county per state.')
     parser.add_argument('-d', '--deaths', help='Add number of deaths to the bar plot.', action='store_true')
     parser.add_argument('-o', '--only-deaths', help='Show only number of deaths in the bar plot.', action='store_true')
+    parser.add_argument('-l', '--log', help='Plot in logarithmic scale.', action='store_true')
     args = vars(parser.parse_args())
-    pandafunc(args['state'], args['county'], args['deaths'], args['only_deaths'])
+
+    pandafunc(args['state'], args['county'], args['deaths'], args['only_deaths'], args['log'])
