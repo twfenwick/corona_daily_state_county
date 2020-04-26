6e574d7c0845110f70030ad26ac89ae7bd418a0a
tim fenwick <twfenwick@gmail.com>
2020-04-26 14:11:20 -0400

    WIP with publishing

--- run.py
+++ run.py
@@ -1,15 +1,20 @@
 import argparse
+import os
 
+import chart_studio
+import chart_studio.plotly as py
+import plotly.graph_objects as go
 import matplotlib.pyplot as plt
+import plotly.express as px
 import pandas
 
 path_counties = '/Users/tim/code/github/nytimes/covid-19-data/us-counties.csv'
 path_states = '/Users/tim/code/github/nytimes/covid-19-data/us-states.csv'
 
-cmdln_override = False
-config_d = False
+cmdln_override = True
+config_d = True
 config_od = False
-config_logy = True
+config_logy = False
 
 """
 When I enter county and state
@@ -44,12 +49,76 @@ def pandafunc(state='North Carolina', county=None, deaths=False, only_deaths=Fal
     else:
         count = 'cases'
 
+    new_cases = df['cases'].diff()
+    new_deaths = df['deaths'].diff()
+
+    plot_data_browser(df, title, new_cases, new_deaths)
+
+    # basic_browser_plot(count, df, logy, title)
+
+    # basic_local_plot(count, df, logy, title)
+
+
+def plot_data_browser(df, title, new_cases, new_deaths):
+    # Browser group plots:
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=df['date'],
+        y=df['cases'],
+        name='Total Cases',
+        marker_color='sandybrown'
+    ))
+    fig.add_trace(go.Bar(
+        x=df['date'],
+        y=new_cases,
+        name='New Cases',
+        marker_color='indianred'
+    ))
+    fig.add_trace(go.Bar(
+        x=df['date'],
+        y=df['deaths'],
+        name='Deaths',
+        marker_color='black'
+    ))
+    fig.add_trace(go.Bar(
+        x=df['date'],
+        y=new_deaths,
+        name='New Deaths',
+        marker_color='gray'
+    ))
+    # Here we modify the tickangle of the xaxis, resulting in rotated labels.
+    fig.update_layout(title=title, barmode='overlay', xaxis_tickangle=-45)
+    # fig.show()
+
+
+
+    import plotly.io as pio
+    pio.write_html(fig, file='index.html', auto_open=True)
+
+    import chart_studio.tools as tls
+    tls.get_embed('file:///Users/tim/code/bitbucket/twfenwick/corona_tracker/index.html')
+
+
+def basic_local_plot(count, df, logy, title):
+    # Local plots:
     df.plot(logy=logy, kind='bar', x='date', y=count, grid=True, title=title)
     plt.show()
     plt.close()
 
 
-if __name__ == '__main__':
+def basic_browser_plot(count, df, logy, title):
+    # Browser plots:
+    fig = px.bar(df, x='date', y=count, title=title, log_y=logy, barmode='overlaid')
+    fig.show()
+
+
+def main():
+    args = parse_args()
+    pull_latest_corona_data()
+    pandafunc(args['state'], args['county'], args['deaths'], args['only_deaths'], args['log'])
+
+
+def parse_args():
     parser = argparse.ArgumentParser(description='Show trend of infection rate per state or county.')
     parser.add_argument('-s', '--state', help='Required for any query.', required=True)
     parser.add_argument('-c', '--county', help='Optional for specific county per state.')
@@ -57,5 +126,12 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--only-deaths', help='Show only number of deaths in the bar plot.', action='store_true')
     parser.add_argument('-l', '--log', help='Plot in logarithmic scale.', action='store_true')
     args = vars(parser.parse_args())
+    return args
 
-    pandafunc(args['state'], args['county'], args['deaths'], args['only_deaths'], args['log'])
+
+def pull_latest_corona_data():
+    os.system('cd /Users/tim/code/github/nytimes/covid-19-data; pwd; git pull origin master')
+
+
+if __name__ == '__main__':
+    main()
